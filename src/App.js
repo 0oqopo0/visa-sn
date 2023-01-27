0cf9ede3a5694bf692462878f41d86a3c1358ed4
M.Sh <72550391+0oqopo0@users.noreply.github.com>
2023-01-27 12:30:09 +0330

    first

--- src/App.js
+++ src/App.js
@@ -50,7 +50,7 @@ const App = () => {
             </TooltipComponent>
           </div>
           {activeMenu ? (
-            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white ">
+            <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-green-200 ">
               <Sidebar />
             </div>
           ) : (
